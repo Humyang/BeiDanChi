d72c20c21acf84e03660519dbef475ac97b978f3
Yangxg <jessebutchman@gmail.com>
2016-10-20 17:50:43 +0800

    update

--- src/components/wordList.vue
+++ src/components/wordList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="Index">
+  <div id="index">
       <navbar title="首页" :left="navbar_btn_left" :right="navbar_btn_right"></navbar>
       <content class="container">
         <loadmore :top-method="loadTop" >
@@ -26,6 +26,23 @@ import loadmore from 'mint-loadmore';
 import drop_down from '../vendor/drop_down.js'
 // import hambraug from './hambraug'
 export default {
+  data () {
+    return {
+      search_text:'',
+      show_hambraug:false,
+      show_search:false,
+      lists:[
+          {describe:'1111111',size:0},
+          {describe:'222222222',size:1},
+          {describe:'aaaaa',size:1},
+          {describe:'a1acxba1aa',size:1},
+          {describe:'aa2qwgzaaa2',size:1},
+          {describe:'aaahjla3a',size:1},
+          {describe:'bbq2bbs4b',size:1},
+          {describe:'cc1caacc3c',size:1}
+      ]
+    }
+  },
   components:{
     navbar,
     card,
@@ -63,32 +80,13 @@ export default {
   },
   events:{
   },
-  data () {
-    return {
-      search_text:'',
-      show_hambraug:false,
-      show_search:false,
-      lists:[
-          {describe:'1111111',size:0},
-          {describe:'222222222',size:1},
-          {describe:'aaaaa',size:1},
-          {describe:'a1acxba1aa',size:1},
-          {describe:'aa2qwgzaaa2',size:1},
-          {describe:'aaahjla3a',size:1},
-          {describe:'bbq2bbs4b',size:1},
-          {describe:'cc1caacc3c',size:1}
-      ]
-    }
-  },
+
   ready:function(){
     var self = this
     drop_down.listen("word_card",function(){
       self.show_search = true
       console.log(true)
     })
-    // var container = document.getElementsByClassName("container")[0];
-    // container.scrollTop=;
-    // console.log(container.scrollTop)
   }
   
 }
@@ -104,24 +102,21 @@ export default {
 </style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#Index { height: 100%; overflow: hidden; }
-.right_btn { }
+#index { height: 100%; overflow: hidden; }
 .container { background-color: #36474F; overflow: auto; display: block; height: 100%; padding-bottom: 50px; }
-
 /* 下拉刷新 */
 section.refresh { color: #ffffff; text-align: center; margin-top: 45px; padding-top: 5px; }
-.refresh p { margin: 10px; }
-.search_wrap {    margin-top: 0.1rem;}
-/* 搜索框 */
-.search_wrap p { background-color: white; margin: 0 auto; width: 6.2rem; height: 45px; border-radius: 2px; }
-.search_wrap img { padding: 14px; float: left; }
-.search_wrap input[type="text"] { font-size: 14px; line-height: 45px; padding: 0; margin: 0; color: #000000; width: 5.2rem; border: 0;}
+section.refresh p { margin: 10px; }
+section.search_wrap {    margin-top: 0.1rem;}
+section.search_wrap p { background-color: white; margin: 0 auto; width: 6.2rem; height: 45px; border-radius: 2px; }
+section.search_wrap img { padding: 14px; float: left; }
+section.search_wrap input[type="text"] { font-size: 14px; line-height: 45px; padding: 0; margin: 0; color: #000000; width: 5.2rem; border: 0;}
 section.word_card { overflow: hidden;  }
-.detail_tabs li { font-size: 18px;float: left; width: 2.13rem; text-align: center; color: white; padding: 15px 0; }
+/*.detail_tabs li { font-size: 18px;float: left; width: 2.13rem; text-align: center; color: white; padding: 15px 0; }
 .detail_tabs ul { overflow: hidden; background-color: #00BBD3;border-radius: 1px; }
 section.detail_tabs { margin-top: 10px; }
 .describe textarea { width: 6.3rem; border: 0; margin: 0; padding: 0; background-color: #36474f; border-bottom: 2px solid #80CBC4; margin-left: 5px; color: white; font-size: 16px; }
 section.detail_container.describe { margin-top: 40px; }
-.detail_tabs li.active { border-bottom: 2px solid #FFFF8C; }
+.detail_tabs li.active { border-bottom: 2px solid #FFFF8C; }*/
 
 </style>
