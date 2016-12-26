17cca0406c3d1f20ab61acc9772aaaaf5d4c8e4d
Yangxg <jessebutchman@gmail.com>
2016-12-26 14:41:44 +0800

    蓝色
    添加具体隐藏天数
    所有单词列表从前往后排序

--- src/api/method.js
+++ src/api/method.js
@@ -1,14 +1,16 @@
-
+// var DAY = require('./constant.js').DAY
+import {DAY} from './constant.js'
 export const timeFormat = function(timestamp){
     // console.log('timestamp',timestamp)
 
     if(timestamp!=undefined){
-        let cueent_time = new Date()
-        let time = new Date(timestamp) 
-        if(cueent_time.getTime() > time){
+        let cueent = new Date()
+        let target = new Date(timestamp) 
+        if(cueent.getTime() > timestamp){
             return 'now'
         }
-        return time.toLocaleDateString() +" "+ time.toLocaleTimeString()
+        let remain = target - cueent
+        return Math.round(remain/DAY)+"天后 "+ target.toLocaleDateString() +" "+ target.toLocaleTimeString()
     }
 
     return "unkonw"
@@ -17,8 +19,6 @@ var CODE = require('../../serve/constant.js').CODE
 export const pageHandle = function(err){
     if( CODE.LOGIN_NO_LOGIN.STATUSCODE === err.STATUSCODE ||
         CODE.LOGIN_TOKEN_INVALID.STATUSCODE === err.STATUSCODE){
-          // console.log('jump')
-          // location.href = '/login'
           this.$router.go('/login')
     }
 }
